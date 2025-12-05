f32e4ae6cc1db5c5e7a0de484b5196994424ee93
THAMILSELVAN2006-git <thamilselvan.p2024cse@sece.ac.in>
2025-12-05 09:06:17 +0530

    Day-4

---
+++ day4.js
@@ -0,0 +1,38 @@
+/*let arr=["john","don","ube"]
+console.log(arr)
+console.log(arr.toString())
+arr.unshift("Muruga")
+console.log(arr)
+arr.unshift("Thamil")
+console.log(arr)
+arr.pop()
+console.log(arr)
+arr.shift()
+console.log(arr)
+arr.splice(0,2,"Thamil","Ube")
+console.log(arr)
+delete arr[0]
+console.log(arr)
+arr.push("Mern Intern")
+console.log(arr)
+arr.splice(0,1)
+console.log(arr)
+let arr1=arr.slice(0,1)
+console.log(arr1)*/
+
+/*let mob={sna:"Thamil",age:19,demo:function()
+    {
+        console.log(("Hi I am"+" "+this.sna+" "+this.age))
+    }
+}
+mob.demo()*/
+
+let stu=[{sna:"Thamil",age:19},{sna:"Ube",age:19},{sna:"Muruga",age:19}]
+stu.forEach((Student)=>
+{
+   console.log(stu)
+}
+)
+
+
+                               
